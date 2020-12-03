f21145f4ea9426f8ca6922d90c4229cf65efc1bf
Alex Heist <a.heist@alexheist.com>
2020-12-03 16:43:07 -0700

    datetime display

--- main/models.py
+++ main/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+import datetime
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Link(models.Model):
+    active = models.BooleanField(default=None)
+    icon = models.ImageField()
+    url = models.URLField()
+
+
+class WordOfTheDay(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date = models.DateField(default=datetime.date.today)
+    word = models.CharField(max_length=127)
+    part_of_speech = models.CharField(max_length=31)
+    pronunciation = models.CharField(max_length=255)
+    definitions = models.TextField()
+    examples = models.TextField()
+
+
+class Prompt(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    prompt = models.CharField(max_length=255)
+
+
+class Entry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    prompt = models.ForeignKey(Prompt, on_delete=models.SET_NULL, null=True)
+    text = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    wordcount = models.PositiveSmallIntegerField()
